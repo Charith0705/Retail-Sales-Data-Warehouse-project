655ffb0b71e96d0fa098e473b1d4ff65dadd7ee0
Balaboina Charith <charith.mudhiraj@gmail.com>
2026-05-06 01:12:54 +0530

    setup finish

--- sql scripts/00_setup.sql
+++ sql scripts/00_setup.sql
@@ -9,16 +9,3 @@ CREATE SCHEMA IF NOT EXISTS sales_dwh.silver;
 CREATE SCHEMA IF NOT EXISTS sales_dwh.gold;
 
 SHOW SCHEMAS IN sales_dwh;
-
-drop table if exists sales_dwh.bronze.raw_customers;
-drop table if exists sales_dwh.bronze.raw_products;
-drop table if exists sales_dwh.bronze.raw_stores;
-drop table if exists sales_dwh.bronze.raw_sales ;
-drop table if exists sales_dwh.silver.dim_customer;
-drop table if exists sales_dwh.silver.dim_product;
-drop table if exists sales_dwh.silver.dim_store;
-drop table if exists sales_dwh.silver.fact_sales;
-drop table if exists sales_dwh.gold.dim_customer;
-drop table if exists sales_dwh.gold.dim_product;
-drop table if exists sales_dwh.gold.dim_store;
-drop table if exists sales_dwh.gold.fact_sales;
